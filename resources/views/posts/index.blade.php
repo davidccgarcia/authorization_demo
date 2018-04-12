a2e104814051bd84251fb51ad23085aab818d5ed
David García <ccristhiangarcia@gmail.com>
2018-04-12 13:31:10 -0500

    Use can directive to the authorization component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,7 +17,13 @@
         <td>{{ $post->id }}</td>
         <td>{{ $post->title }}</td>
         <td>{{ $post->user->name }}</td>
-        <td><a href="{{ url('edit-post', [$post->id]) }}">Editar</a></td>
+        <td>
+            @can('update_post', $post)
+                <a href="{{ url('posts/edit', [$post->id]) }}">
+                    Editar
+                </a>
+            @endcan
+        </td>
     </tr>
     @endforeach
 </table>
